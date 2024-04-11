2bb83b949a0184fb40eea11520f99c2846fad6e5
student <abastola@pgh.yearup.org>
2024-04-11 13:56:38 -0400

    initial commit

---
+++ java-development/workbook -1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("ShoppingList");
+        System.out.println("milk");
+        System.out.println("bread");
+        System.out.println("eggs");
+        System.out.println("candy");
+        System.out.println("water");
+        System.out.println("coffee");
+        System.out.println("kiwi");
+        System.out.println("orange");
+        System.out.println("apples");
+        System.out.println("grapes");
+        
+
+    }
+}
