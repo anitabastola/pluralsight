709f4358564926c2749075fdcf1840919e32f3af
student <abastola@pgh.yearup.org>
2024-04-12 11:56:11 -0400

    Added a new folder for practice

---
+++ practice-projects/intro-to-methods/src/main/java/com/pluralsight/main.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class main {
+
+    public static void main(String[] args) {
+        displayName();
+        displayName();
+        displayName();
+        displayName();
+
+        displayFruits();
+        displayFruits();
+    }
+       public static void displayName() {
+           System.out.println("anita");
+           System.out.println("Bastola");
+       }
+
+
+        public static void displayFruits() {
+            System.out.println("Orange");
+            System.out.println("Strawberry");
+
+
+        }
+}
